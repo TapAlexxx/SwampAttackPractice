5529ab170c22b27fa87c2a2c6f64b46184af7b46
TapAlexxx <lordpictures123@gmail.com>
2020-10-13 17:20:22 +0300

    StateMachine

---
+++ SwampAttackPractice/Assets/Skripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private int _damage;
+    [SerializeField] private float _speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * _speed * Time.deltaTime, Space.World);           
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Enemy enemy))
+        {
+            enemy.TakeDamage(_damage);
+            Destroy(gameObject);
+        }
+    }
+}
